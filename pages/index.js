48720ad5fab39ee5a23b9a3f33272b3c8fd1cd8d
matthanes <hanes.matt@gmail.com>
2021-03-11 16:29:27 -0500

    Add meta description

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="flex justify-center">
       <Head>
         <title>Schomburg Road Baptist Church Columbus Georgia - Home</title>
-        <meta name='keywords' content='church, family-friendly, baptist, small, kids, nursery'/>
+        <meta
+          name="keywords"
+          content="church, family-friendly, baptist, small, kids, nursery"
+        />
+        <meta
+          name="description"
+          content="Loving Him, loving them, changing lives! That is our purpose at Schomburg Road Baptist Church. Everything we do is driven from these six short words written above. By understanding this statement you will better understand who we are."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -13,5 +20,5 @@ export default function Home() {
         <p className="text-blue-600">This should be very blue.</p>
       </div>
     </div>
-  )
+  );
 }
