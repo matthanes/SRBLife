67d0e49d3336c20ebe0a54bf4331d394a96bf266
matthanes <hanes.matt@gmail.com>
2021-03-12 13:40:34 -0500

    Salvation mispelling

--- pages/salvation.js
+++ pages/salvation.js
@@ -4,13 +4,13 @@ export default function Salvation() {
   return (
     <div className="flex justify-center">
       <Head>
-        <title>Schomburg Road Baptist Church Columbus Georgia - Salavation</title>
+        <title>Schomburg Road Baptist Church Columbus Georgia - Salvation</title>
         <meta name='keywords' content='beliefs, doctrine'/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <div className="mt-4 p-4 w-1/4 rounded bg-blue-300 text-center">
-        <p className="text-blue-600">Salavation</p>
+        <p className="text-blue-600">Salvation</p>
       </div>
     </div>
   )
