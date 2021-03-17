8836da211f9718b7a6f6a0fb7e1d9fb9b14229be
matthanes <hanes.matt@gmail.com>
2021-03-17 10:52:33 -0400

    Add secondary color on hover

--- components/Homecard.js
+++ components/Homecard.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Homecard = ({title, subtitle, icon, target, href}) => {
   return (
     <a href={href} target={target} className="cursor-pointer my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3 text-center">
-      <article className="py-8 overflow-hidden rounded-lg shadow-lg bg-primary text-white">
+      <article className="py-8 overflow-hidden rounded-lg shadow-lg hover:bg-secondary bg-primary text-white">
         <FontAwesomeIcon className="block mx-auto" fixedWidth size="6x" icon={icon} />
         <div className="py-5">
           <div className="py-2 font-bold text-2xl mb-2">{title}</div>
