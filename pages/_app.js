ae135eb8890a7ca1c1587504b51a2f8a8cf60ffb
matthanes <hanes.matt@gmail.com>
2021-03-18 09:40:22 -0400

    remove fontawesome library ref

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
-library.add(fab, faMapMarkedAlt, faClock, faPhone, faEnvelope, faCalendar);
-
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
