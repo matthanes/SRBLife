b6e50b877aad27ec3feec56b88b93184d828c300
matthanes <hanes.matt@gmail.com>
2021-09-26 20:40:44 -0400

    PWA Support added

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   trailingSlash: true,
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
