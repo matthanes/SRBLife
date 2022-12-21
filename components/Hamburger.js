d7be56d8c9f12d5de895aefbc34ac37f4d7d9939
Matt Hanes <hanes.matt@gmail.com>
2022-12-20 19:36:19 -0500

    New mobile nav

---
+++ components/Hamburger.js
@@ -0,0 +1,30 @@
+const Hamburger = ({handleClick, active}) => {
+  return (
+    <button
+            className="flex flex-col p-3 hover:bg-secondary rounded lg:hidden text-white hover:text-white outline-none"
+            onClick={handleClick}
+          >
+            <div
+              className={`h-1 w-6 my-[.1875rem] rounded-full bg-white transition ease transform duration-300 ${
+                active
+                  ? "rotate-45 translate-y-[.625rem] opacity-100 group-hover:opacity-100"
+                  : "opacity-100 group-hover:opacity-100"
+              }`}
+            />
+            <div
+              className={`h-1 w-6 my-[.1875rem] rounded-full bg-white transition ease transform duration-300 ${
+                active ? "opacity-0" : "opacity-100 group-hover:opacity-100"
+              }`}
+            />
+            <div
+              className={`h-1 w-6 my-[.1875rem] rounded-full bg-white transition ease transform duration-300 ${
+                active
+                  ? "-rotate-45 -translate-y-[.625rem] opacity-100 group-hover:opacity-100"
+                  : "opacity-100 group-hover:opacity-100"
+              }`}
+            />
+          </button>
+  )
+}
+
+export default Hamburger
--- components/Nav2.js
+++ components/Nav2.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import Hamburger from "./Hamburger";
 
 const links = [
   { name: "Home", target: "/" },
@@ -11,56 +12,20 @@ const links = [
 ];
 
 const Nav2 = () => {
-  const [navOpen, setNavOpen] = useState(false);
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
 
   return (
     <header className="bg-primary sticky top-0 z-10">
       <nav>
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex items-center justify-between h-16">
-            <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
+            <div className="absolute inset-y-0 right-0 flex items-center lg:hidden">
               {/* Mobile menu button */}
-              <button
-                type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md text-gray-100 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-                onClick={() => setNavOpen(!navOpen)}
-              >
-                <span className="sr-only">Open main menu</span>
-                {/*Icon when menu is closed. Heroicon name: outline/menu Menu open: "hidden", Menu closed: "block"*/}
-                <svg
-                  className={"block h-6 w-6" + (navOpen ? " hidden" : "")}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-                {/*Icon when menu is open. Heroicon name: outline/x Menu open: "block", Menu closed: "hidden"*/}
-                <svg
-                  className={"block h-6 w-6" + (navOpen ? "" : " hidden")}
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
+              <Hamburger handleClick={handleClick} active={active} />
             </div>
             <div className="flex-1 flex items-center justify-center lg:items-stretch lg:justify-between">
               <div className="flex-shrink-0 flex items-center">
@@ -90,13 +55,13 @@ const Nav2 = () => {
             </div>
           </div>
         </div>
-        <div className={(navOpen ? "" : " hidden") + " lg:hidden"} id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1">
+        <div className={(active ? " bg-primary w-full h-screen" : " hidden") + " lg:hidden"} id="mobile-menu">
+          <div className="px-2 pt-2 pb-3 space-y-1 mt-16">
             {links.map((link) => (
               <li
-                className="text-center list-none text-gray-100 hover:bg-secondary hover:text-white block px-3 py-2 rounded-md font-rock-salt text-lg font-semibold"
+                className="text-center list-none select-none text-gray-100 hover:bg-secondary hover:text-white block px-3 py-2 rounded-md font-rock-salt text-lg font-semibold"
                 key={link.name}
-                onClick={() => setNavOpen(!navOpen)}
+                onClick={() => setActive(!active)}
               >
                 <Link href={link.target} as={link.as}>{link.name}</Link>
               </li>
