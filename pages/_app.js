273e77cff8c9f78e923754781d285edff50e2314
Matt Hanes <hanes.matt@gmail.com>
2023-01-18 19:17:37 -0500

    Add route change handler for GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import Script from 'next/script';
 import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Script
