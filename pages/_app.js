fafa5937060383abca30599c3ceb0dd68ba69e83
Matt Hanes <hanes.matt@gmail.com>
2023-03-06 19:42:46 -0500

    Add key to re-render on route change.

--- pages/_app.js
+++ pages/_app.js
@@ -23,15 +23,19 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      {/* Conditionally render the next two <Script /> tags in production only */}
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+      )}
+      {process.env.NODE_ENV === 'production' && (
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -39,10 +43,11 @@ function MyApp({ Component, pageProps }) {
               page_path: window.location.pathname,
             });
             `,
-        }}
-      />
+          }}
+        />
+      )}
       <Layout>
-        <Component {...pageProps} />
+        <Component {...pageProps} key={router.asPath} />
       </Layout>
     </>
   );
