f86c29b89f0939b14e9089a0c69b589f8dc148f0
Matt Hanes <hanes.matt@gmail.com>
2023-06-11 14:27:18 -0400

    Image adjustments for splits on mobile

--- components/SplitScreen.js
+++ components/SplitScreen.js
@@ -4,25 +4,22 @@ import imgSrcSet from '../utilities/srcset';
 import { Markup } from 'react-render-markup';
 
 const SplitScreen = ({ img, alt, title, body, reverse }) => {
-  const classes = classNames({
-    'flex': true,
-    'w-full': true,
-    'flex-col': true,
+  const classes = classNames('flex w-full flex-col',{
     'xl:flex-row-reverse': reverse,
     'xl:flex-row': !reverse,
   });
   return (
     <div className={classes}>
-      <div className="flex justify-stretch bg-gradient-to-br from-primary to-indigo-600 xl:basis-1/2">
+      <div className="flex bg-gradient-to-br from-primary to-indigo-600 xl:basis-1/2">
         <img
-          className="object-cover"
+          className="object-cover min-w-full min-h-1/2"
           src={img}
           alt={alt}
           srcSet={imgSrcSet(img)}
           sizes='(min-width: 1280px) 50vw, 100vw'
         />
       </div>
-      <div className="flex items-center justify-start overflow-hidden p-16 xl:basis-1/2">
+      <div className="flex overflow-hidden p-16 xl:basis-1/2">
         <div className="font-bodytext text-lg">
           {title && <h2 className="mb-6 text-3xl font-bold">{title}</h2>}
           <Markup markup={body} />
